a148f04504f3b3051cba95a695b8b6560875b196
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-06 15:47:54 +0330

    convert link to spa link with react-router-dom

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LineStyleIcon from "@mui/icons-material/LineStyle";
 import TimelineIcon from "@mui/icons-material/Timeline";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
@@ -19,66 +20,96 @@ export default function SideBar() {
       <div className="sideBar__menu">
         <h3 className="sidebar__title">Dashboard</h3>
         <ul className="sidebar__list">
-          <li className="SidebarList__item active">
-            <LineStyleIcon className="sideBar__Icon" /> Home
-          </li>
-          <li className="SidebarList__item">
-            <TimelineIcon className="sideBar__Icon" /> Analyses
-          </li>
-          <li className="SidebarList__item">
-            <TrendingUpIcon className="sideBar__Icon" /> Sales
-          </li>
+          <Link className="link" to="/">
+            <li className="SidebarList__item active">
+              <LineStyleIcon className="sideBar__Icon" /> Home
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <TimelineIcon className="sideBar__Icon" /> Analyses
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <TrendingUpIcon className="sideBar__Icon" /> Sales
+            </li>
+          </Link>
         </ul>
       </div>
 
       <div className="sideBar__menu">
         <h3 className="sidebar__title">Quick Menu</h3>
         <ul className="sidebar__list">
-          <li className="SidebarList__item">
-            <PermIdentityIcon className="sideBar__Icon" /> Users
-          </li>
-          <li className="SidebarList__item">
-            <StorefrontIcon className="sideBar__Icon" /> NewUsers
-          </li>
-          <li className="SidebarList__item">
-            <AttachMoneyIcon className="sideBar__Icon" /> Products
-          </li>
-          <li className="SidebarList__item">
-            <BarChartIcon className="sideBar__Icon" /> Transactions
-          </li>
-          <li className="SidebarList__item">
-            <ReportIcon className="sideBar__Icon" /> Reports
-          </li>
+          <Link to="/users" className="link">
+            <li className="SidebarList__item">
+              <PermIdentityIcon className="sideBar__Icon" /> Users
+            </li>
+          </Link>
+
+          <Link to="/newUser" className="link">
+            <li className="SidebarList__item">
+              <StorefrontIcon className="sideBar__Icon" /> NewUsers
+            </li>
+          </Link>
+
+          <Link to="/products" className="link">
+            <li className="SidebarList__item">
+              <AttachMoneyIcon className="sideBar__Icon" /> Products
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <BarChartIcon className="sideBar__Icon" /> Transactions
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <ReportIcon className="sideBar__Icon" /> Reports
+            </li>
+          </Link>
         </ul>
       </div>
 
       <div className="sideBar__menu">
         <h3 className="sidebar__title">Notifications</h3>
         <ul className="sidebar__list">
-          <li className="SidebarList__item">
-            <MailOutlineIcon className="sideBar__Icon" /> Mail
-          </li>
-          <li className="SidebarList__item">
-            <DynamicFeedIcon className="sideBar__Icon" /> FeedBack
-          </li>
-          <li className="SidebarList__item">
-            <ChatBubbleIcon className="sideBar__Icon" /> Message
-          </li>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <MailOutlineIcon className="sideBar__Icon" /> Mail
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <DynamicFeedIcon className="sideBar__Icon" /> FeedBack
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <ChatBubbleIcon className="sideBar__Icon" /> Message
+            </li>
+          </Link>
         </ul>
       </div>
 
       <div className="sideBar__menu">
         <h3 className="sidebar__title">Dashboard</h3>
         <ul className="sidebar__list">
-          <li className="SidebarList__item">
-            <LineStyleIcon className="sideBar__Icon" /> Manage
-          </li>
-          <li className="SidebarList__item">
-            <WorkOutlineIcon className="sideBar__Icon" /> Analytics
-          </li>
-          <li className="SidebarList__item">
-            <ReportIcon className="sideBar__Icon" /> Reports
-          </li>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <LineStyleIcon className="sideBar__Icon" /> Manage
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <WorkOutlineIcon className="sideBar__Icon" /> Analytics
+            </li>
+          </Link>
+          <Link className="link">
+            <li className="SidebarList__item">
+              <ReportIcon className="sideBar__Icon" /> Reports
+            </li>
+          </Link>
         </ul>
       </div>
     </div>
