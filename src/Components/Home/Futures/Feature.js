4ac75361e2f5245d23afa0b6da5b8a95368ee1c5
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-06 20:59:14 +0330

    edit features.js

--- src/Components/Home/Futures/Feature.js
+++ src/Components/Home/Futures/Feature.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./Feature.css";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 export default function Feature() {
   return (
     <div className="features">
-
       <div className="FeatureItem">
         <span className="featureItem__title">Revanue</span>
         <div className="featureItem__container">
           <span className="feature__Money">$2,415</span>
           <span className="feature__Rate">
-            -11.5 <MailOutlineIcon className="feature__icon negative" />
+            -11.5 <ArrowUpwardIcon className="feature__icon " />
           </span>
         </div>
         <span className="featureItem__sub">Compared to last month</span>
       </div>
-      
+
       <div className="FeatureItem">
         <span className="featureItem__title">Sales</span>
         <div className="featureItem__container">
           <span className="feature__Money">$2,415</span>
           <span className="feature__Rate">
-            -11.5 <MailOutlineIcon className="feature__icon negative" />
+            -11.5 <ArrowUpwardIcon className="feature__icon " />
           </span>
         </div>
         <span className="featureItem__sub">Compared to last month</span>
@@ -32,12 +31,11 @@ export default function Feature() {
         <div className="featureItem__container">
           <span className="feature__Money">$2,415</span>
           <span className="feature__Rate">
-            -11.5 <MailOutlineIcon className="feature__icon negative" />
+            -11.5 <ArrowDownwardIcon className="feature__icon negative" />
           </span>
         </div>
         <span className="featureItem__sub">Compared to last month</span>
       </div>
-
     </div>
   );
 }
