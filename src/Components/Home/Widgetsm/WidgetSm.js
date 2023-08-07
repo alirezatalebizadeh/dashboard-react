d210feb6b1ef58c72ea0ac22249817da5116970d
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-07 11:07:25 +0330

    fix error about sintax

--- src/Components/Home/Widgetsm/WidgetSm.js
+++ src/Components/Home/Widgetsm/WidgetSm.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Visibility from "@mui/icons-material/Visibility";
 import './widgetSm.css'
-
+import { newMembers } from './../../../Data/datas'
 
 
 
 export default function widgetSm() {
-  return (
-     <div className='widgetSm'>
+    return (
+        <div className='widgetSm'>
             <span className='widgetSm__Title'>New Join Members</span>
             <ul className='widgetSm__List'>
-                <li widgetSm__List__item>
+                <li li widgetSm__List__item>
                     <img src='logo192.png' alt='widgetImg' className='widgetSm__Img' />
                     <div className='widgetSmUser'>
                         <span className='widgetSmUser__name'>Alireza Talebizadeh</span>
-                        <span className='widgetSmUser__title'>web designer</span>
+                        <span className='widgetSmUser__title'> Web Designer</span>
                     </div>
                     <button className='WidgetSm__Button'>
                         <Visibility className='WidgetSmIcon' />
@@ -22,5 +22,6 @@ export default function widgetSm() {
                 </li>
             </ul>
         </div>
-  )
+    )
+    //FixIt
 }
--- src/Data/datas.js
+++ src/Data/datas.js
@@ -88,4 +88,4 @@ let newMembers = [
   },
 ]
 
-export default { xAxiosData, newMembers };
+export default  {xAxiosData ,newMembers};
